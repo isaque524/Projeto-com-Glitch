45af517011f20c092a21e912be567b712a9b68ce
Glitch (sharp-hill-airedale) <none>
2022-06-16 21:07:15 +0000

    🍁🐿 Checkpoint ./src/Paginas/jogos.hbs:42598709/10 ./src/ctrlJogoVelha.js:42598709/2272 ./src/logicaJogoVelha.js:42598709/1134 ./src/Paginas/jogoVelha.hbs:42598709/10

--- src/ctrlJogoVelha.js
+++ src/ctrlJogoVelha.js
@@ -1,74 +1,16 @@
-// iniciar as variaveis
-let board =['','','','','','','','','',];
-let playertime = 0;
-let playernome =['Grifinória','Sonserina'];
-let symbols = ['o','x'];
-let gameOver = false;
-let winstate = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-]
+const seo = require("./seo.json");
+const db  = require("./sqlite.js");
+const Base64 = require("js-base64");
 
-
-
-function handleMove(position) {
-
-    if(gameOver){
-        return;
-    }
-
-if(board[position]==''){
-    board[position] = symbols[playertime];
-
-    gameOver = isWin();
-
-        if(gameOver == false){
-         if(playertime==0){
-             playertime=1;
-             
-         }else{
-            playertime=0;
-        
-         }
-         playernome = (playertime==0)?'Grifinória':'Sonserina';
-            
-        }
-   
-    }
-    
-
-    return gameOver;
-}
-
-function isWin(){
-
-    for( let i =0; i<winstate.length;i++){
-      
-        let seq = winstate[i];
-
-        let pos1 = seq[0];
-        let pos2 = seq[1];
-        let pos3 = seq[2];
-
-        if(board[pos1] ==  board[pos2] &&
-            board[pos1] == board[pos3]&&
-            board[pos1] != ''){
-
-                return true;
-            }
-
-    }
-    return false;
+module.exports = {
   
+  configurar: async(servidor) => {
+    servidor.get("/jogoVelha", module.exports.viewCadastro);
+    servidor.post("/jogoVelha", module.exports.validarCadastro);
+  },
 
-}
- viewJogos: async(request, reply) => {
+
+viewJogoVelha: async(request, reply) => {
     console.log("Pagina Inicial GET /jogos");
     let params = { seo: seo };
     let valido = await cookie.validacao(request.cookies.Autenticacao);
@@ -80,3 +22,6 @@ function isWin(){
     
     reply.view("/src/Paginas/jogosVelha.hbs", params);
   },
+
+}
+ 
