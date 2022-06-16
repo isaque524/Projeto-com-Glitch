a2be1b514f329755d06c4248df92266f4e9da88d
Glitch (sharp-hill-airedale) <none>
2022-06-16 14:35:47 +0000

    🏇🏉 Checkpoint ./src/Paginas/harry.hbs:42598709/1150 ./src/ctrlJogoVelha.js:42598709/1129 ./src/interfaceJogoVelha:42598709/1214 ./src/Paginas/jogoVelha.hbs:42598709/1130

---
+++ src/ctrlJogoVelha.js
@@ -0,0 +1,69 @@
+// iniciar as variaveis
+let board =['','','','','','','','','',];
+let playertime = 0;
+let playernome =['Grifinória','Sonserina'];
+let symbols = ['o','x'];
+let gameOver = false;
+let winstate = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+
+
+function handleMove(position) {
+
+    if(gameOver){
+        return;
+    }
+
+if(board[position]==''){
+    board[position] = symbols[playertime];
+
+    gameOver = isWin();
+
+        if(gameOver == false){
+         if(playertime==0){
+             playertime=1;
+             
+         }else{
+            playertime=0;
+        
+         }
+         playernome = (playertime==0)?'Grifinória':'Sonserina';
+            
+        }
+   
+    }
+    
+
+    return gameOver;
+}
+
+function isWin(){
+
+    for( let i =0; i<winstate.length;i++){
+      
+        let seq = winstate[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if(board[pos1] ==  board[pos2] &&
+            board[pos1] == board[pos3]&&
+            board[pos1] != ''){
+
+                return true;
+            }
+
+    }
+    return false;
+
+}
